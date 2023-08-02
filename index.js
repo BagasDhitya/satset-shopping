6c470f3baf72cbc3b5d46a0b68eb946105584e43
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2023-08-02 12:52:44 +0700

    setting up cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const parser = require("body-parser");
+const cors = require("cors");
 const { Product } = require("./models");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(parser.json());
 
 // create product
