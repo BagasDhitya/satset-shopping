2047e297e0137d3f73460f5e0f2f0d13d05360a6
bagasdhitya98 <bagas.dhitya@alterra.id>
2023-09-13 22:34:48 +0700

    refactor: solving bug on models

--- models/index.js
+++ models/index.js
@@ -1,21 +1,21 @@
+'use strict';
+
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname +'/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], {
-    dialect: 'mysql',
-    dialectModule: 'mysql2'
+    dialect: 'mysql'
   });
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, {
-    host: config.host,
     dialect: 'mysql',
     dialectModule: 'mysql2'
   });
