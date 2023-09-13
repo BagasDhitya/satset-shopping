bcd1c43137b0bf05afd4482d23cd1b7c6d15b0b6
bagasdhitya98 <bagas.dhitya@alterra.id>
2023-09-13 22:46:07 +0700

    readd: config port

--- api/index.js
+++ api/index.js
@@ -4,8 +4,13 @@ const cors = require("cors");
 const productRoutes = require("../routes/productRoutes");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(parser.json());
 
 app.use("/api", productRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
